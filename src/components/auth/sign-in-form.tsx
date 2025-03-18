ec0d998cf0d5170b407fe1bc91ffa285f8e7e9a7
RaulCudris <raulcudriz@hotmail.com>
2025-03-18 12:13:10 -0500

    Modificacion en el nav

--- src/components/auth/sign-in-form.tsx
+++ src/components/auth/sign-in-form.tsx
@@ -29,7 +29,7 @@ const schema = zod.object({
 
 type Values = zod.infer<typeof schema>;
 
-const defaultValues = { email: 'sofia@devias.io', password: 'Secret1' } satisfies Values;
+const defaultValues = { email: 'apploochon@gmail.com', password: 'Secret1' } satisfies Values;
 
 export function SignInForm(): React.JSX.Element {
   const router = useRouter();
@@ -151,7 +151,7 @@ export function SignInForm(): React.JSX.Element {
         <Typography component="span"
                     sx={{ fontWeight: 700 }}
                     variant="inherit">
-          sofia@devias.io
+          apploochon@gmail.com
         </Typography>{' '}
         with password{' '}
         <Typography component="span"
